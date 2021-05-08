5aede05d236578e0931451c7ca326fd0e502f921
Your Name <you@example.com>
2021-05-08 14:58:09 +0700

    fix geo 2

--- plugins/axios.js
+++ plugins/axios.js
@@ -6,6 +6,10 @@ process.env.NODE_TLS_REJECT_UNAUTHORIZED = '0'
 export default ({ app, store, redirect, req }) => {
   axios.defaults.baseURL = process.env.apiUrl
 
+  if (process.server) {
+    return
+  }
+
   // Request interceptor
   axios.interceptors.request.use((request) => {
   //   const token = store.getters['auth/token']
@@ -24,10 +28,6 @@ export default ({ app, store, redirect, req }) => {
 
     return request
   })
-
-  if (process.server) {
-    return
-  }
 
   // Response interceptor
   axios.interceptors.response.use(response => response, (error) => {
