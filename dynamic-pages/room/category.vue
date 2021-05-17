4b745df44ae6b776214b2e5fe7544d5808577fb7
Your Name <you@example.com>
2021-05-17 14:43:44 +0700

    fix some errors in catalog

--- dynamic-pages/room/category.vue
+++ dynamic-pages/room/category.vue
@@ -6,188 +6,176 @@
 
 		<div class="rooms__catalog">
 
-        <client-only>
+      <client-only>
 
-          <filter-header v-if="rooms"
-            class="rooms__filter-header"
-            :geo.sync="geo"
-            :sort.sync="sort"
-            :total.sync="total"
-            :overall.sync="overall"
-            :sort-options="sortOptions"
-            entity-label="покер-румов"
-            @update:sort="fetchItems"
-            @update:geo="fetchItems"
+        <filter-header v-if="rooms"
+          class="rooms__filter-header"
+          :geo.sync="geo"
+          :sort.sync="sort"
+          :total.sync="total"
+          :overall.sync="overall"
+          :sort-options="sortOptions"
+          entity-label="покер-румов"
+          @update:sort="fetchItems"
+          @update:geo="fetchItems"
+        />
+
+        <filter-selected-list v-if="selected.length">
+          <filter-selected v-for="(item, index) in selected" :key="index"
+            :label="item.label"
+            :value="item.value"
+            :item-key="item.key"
           />
+          <filter-selected
+            label="Очистить фильтры"
+            :clear="true"
+            :value="null"
+            :key="null"
+          />
+        </filter-selected-list>
+      </client-only>
 
-          <filter-selected-list v-if="selected.length">
-            <filter-selected v-for="(item, index) in selected" :key="index"
-              :label="item.label"
-              :value="item.value"
-              :item-key="item.key"
-            />
-            <filter-selected
-              label="Очистить фильтры"
-              :clear="true"
-              :value="null"
-              :key="null"
-            />
-          </filter-selected-list>
-
-        </client-only>
-
-					<div class="rooms-list">
-
-						<template v-if="rooms" v-for="(item, index) in rooms">
-							<room
-								:key="index"
-								:id="item.id"
-								:title="item.title"
-								:slug="item.slug"
-								:rating="item.rating"
-								:rakeback="item.rakeback"
-								:bonus="item.bonus"
-								:background="item.background"
-								:restricted="item.restricted"
-								:available="item.available"
-								:blacklist="item.blacklist"
-								:summary="item.summary"
-								:claim_amount="item.claim_amount"
-								:claim_currency="item.claim_currency"
-								:image="item.image"
-								:network="item.network"
-								:tags="item.tags"
-								:review="item.review"
-							/>
-							<room-category-banner :key="index + 6" v-if="index > 3 && index < 5" />
-						</template>
-
-						<pagination
-							v-if="rooms.length"
-							:query="true"
-							:last="last_page"
-							:current="parseInt(page)"
-							:prev-url="prev_page_url"
-							:next-url="next_page_url"
-							:total="total"
-							:from="from"
-							:to="to"
-							:load-more-width="208"
-							load-more-text="Показать еще румы"
-							total-text="покер-румов"
-							@next="handlePageNext"
-							@prev="handlePagePrev"
-							@change="handlePageChange"
-							@more="handleShowMore">
-						</pagination>
-					</div>
-
-						<!-- Toc -->
-          <div class="rooms__toc">
-
-            <toc-list v-if="category.toc">
-              <template v-slot="{ inline }">
-                <toc-item v-for="(item, index) in category.toc" :key="index"
-                  :index="index"
-                  :inline="inline"
-                  :anchor="item.anchor_id"
-                  :text="item.text">
-                </toc-item>
-              </template>
-            </toc-list>
-
-          </div>
-
-          <div class="rooms__info">
-            <!-- Article -->
-            <page-article :title="false" :text="category.text">
-
-              <template v-slot:footer>
-                <!-- Faq -->
-                <faq-list v-if="category.faq && category.faq.mainEntity.length" label="FAQ">
-                  <faq-item v-for="(item, index) in category.faq.mainEntity" :key="index"
-                    :question="item.name"
-                    :answer="item.acceptedAnswer.text">
-                  </faq-item>
-                </faq-list>
-
-                <!-- Author -->
-                <author v-if="category.author" :author="category.author" />
-
-                <!-- Comments -->
-                <comments commentable_type="App\RoomCategory" :commentable_id="category.id"/>
-              </template>
-
-            </page-article>
-          </div>
-
-				<div class="rooms__category-filters">
-
-					<room-category-filters
-						v-if="filters"
-						:kycs="filters.kycs"
-						:platforms="filters.platforms"
-						:tags="filters.tags"
-						:payments="filters.payments"
-						:types="filters.types"
-						:networks="filters.networks"
-						:licenses="filters.licenses"
-						:limits="filters.limits"
-						:disciplines="filters.disciplines"
-						:games="filters.games"
-						:huds="filters.huds"
-						:certificates="filters.certificates"
-						@change="handleFilterChange"
-						@filterOpen="handleFilterOpen"
+			<div class="rooms-list">
+				<template v-if="rooms.length" v-for="(item, index) in items">
+					<room
+            v-if="!item.banner"
+            :key="index"
+						:id="item.id"
+						:title="item.title"
+						:slug="item.slug"
+						:rating="item.rating"
+						:rakeback="item.rakeback"
+						:bonus="item.bonus"
+						:background="item.background"
+						:restricted="item.restricted"
+						:available="item.available"
+						:blacklist="item.blacklist"
+						:summary="item.summary"
+						:claim_amount="item.claim_amount"
+						:claim_currency="item.claim_currency"
+						:image="item.image"
+						:network="item.network"
+						:tags="item.tags"
+						:review="item.review"
 					/>
 
-					<div class="block-title">Последние акции</div>
+          <room-category-banner :key="index" v-else />
+				</template>
 
-					<promotion-item
-						v-if="promotions"
-						v-for="(item, index) in promotions" :key="index"
-						:image="item.image"
+				<pagination
+					v-if="rooms.length"
+					:query="true"
+					:last="last_page"
+					:current="parseInt(page)"
+					:prev-url="prev_page_url"
+					:next-url="next_page_url"
+					:total="total"
+					:from="from"
+					:to="to"
+					:load-more-width="208"
+					load-more-text="Показать еще румы"
+					total-text="покер-румов"
+					@next="handlePageNext"
+					@prev="handlePagePrev"
+					@change="handlePageChange"
+					@more="handleShowMore">
+				</pagination>
+			</div>
+
+      <div class="rooms__toc">
+
+        <toc-list v-if="category.toc">
+          <template v-slot="{ inline }">
+            <toc-item v-for="(item, index) in category.toc" :key="index"
+              :index="index"
+              :inline="inline"
+              :anchor="item.anchor_id"
+              :text="item.text">
+            </toc-item>
+          </template>
+        </toc-list>
+      </div>
+
+      <div class="rooms__info">
+
+        <page-article :title="false" :text="category.text">
+
+          <template v-slot:footer>
+
+            <faq-list v-if="category.faq && category.faq.mainEntity.length" label="FAQ">
+              <faq-item v-for="(item, index) in category.faq.mainEntity" :key="index"
+                :question="item.name"
+                :answer="item.acceptedAnswer.text">
+              </faq-item>
+            </faq-list>
+
+            <author v-if="category.author" :author="category.author" />
+
+            <comments commentable_type="App\RoomCategory" :commentable_id="category.id"/>
+          </template>
+
+        </page-article>
+      </div>
+
+			<div class="rooms__category-filters">
+
+				<room-category-filters
+					v-if="filters"
+					:kycs="filters.kycs"
+					:platforms="filters.platforms"
+					:tags="filters.tags"
+					:payments="filters.payments"
+					:types="filters.types"
+					:networks="filters.networks"
+					:licenses="filters.licenses"
+					:limits="filters.limits"
+					:disciplines="filters.disciplines"
+					:games="filters.games"
+					:huds="filters.huds"
+					:certificates="filters.certificates"
+					@change="handleFilterChange"
+					@filterOpen="handleFilterOpen"
+				/>
+
+				<div class="block-title">Последние акции</div>
+
+				<promotion-item
+					v-if="promotions"
+					v-for="(item, index) in promotions" :key="index"
+					:image="item.image"
+					:title="item.title"
+					:summary="item.summary"
+					:page="item.page"
+					:author="item.author"
+					:created="item.created_at"
+					:category="item.category"
+					:time_left="item.time_left"
+					:time_before="item.time_before"
+					:prize="item.prize"
+					:currency="item.currency ? item.currency.symbol: '$'"
+					:exclusive="item.exclusive"
+				></promotion-item>
+
+				<topic-list v-if="category.topics.length">
+					<topic-item
+						v-for="(item, index) in category.topics" :key="index"
 						:title="item.title"
-						:summary="item.summary"
-						:page="item.page"
+						:url="item.url"
 						:author="item.author"
 						:created="item.created_at"
-						:category="item.category"
-						:time_left="item.time_left"
-						:time_before="item.time_before"
-						:prize="item.prize"
-						:currency="item.currency ? item.currency.symbol: '$'"
-						:exclusive="item.exclusive"
-					></promotion-item>
+					/>
+				</topic-list>
 
-					<topic-list v-if="category.topics.length">
-						<topic-item
-							v-for="(item, index) in category.topics" :key="index"
-							:title="item.title"
-							:url="item.url"
-							:author="item.author"
-							:created="item.created_at"
-						/>
-					</topic-list>
-
-					<game-search-banner />
-
-				</div>
-
-
+				<game-search-banner />
+			</div>
 		</div>
-
 		<page-banners />
-
 	</div>
 </template>
 
 <script>
 
 import { mapGetters } from 'vuex'
-import axios from 'axios'
-
-import Room from '~/components/cards/Room'
 
 export default {
 
@@ -207,7 +195,7 @@ export default {
 	},
 
 	components: {
-		Room
+
 	},
 
 	computed: {
@@ -234,7 +222,6 @@ export default {
 				locale: this.locale,
 				geo: this.geo,
 				room_category_id: this.category.id,
-				ids: this.category.list,
 				types: this.types,
 				payments: this.payments,
 				platforms: this.platforms,
@@ -250,7 +237,15 @@ export default {
 				hud: this.hud,
 				kyc: this.kyc
 			}
-		}
+		},
+
+    items() {
+      return [
+        ...this.rooms.slice(0, this.rooms.length / 2),
+        { banner: true },
+        ...this.rooms.slice(this.rooms.length / 2, this.rooms.length)
+      ]
+    }
 	},
 
 	data: () => ({
@@ -285,14 +280,14 @@ export default {
 		total: 0,
 		overall: 0,
 		countries: [],
-	selected: [],
-	sortOptions: [{
-	  label: 'Сначала лучшие',
-	  value: 'rating'
-	},{
-	  label: 'Сначала новые',
-	  value: 'created_at'
-	}]
+  	selected: [],
+  	sortOptions: [{
+  	  label: 'Сначала лучшие',
+  	  value: 'rating'
+  	},{
+  	  label: 'Сначала новые',
+  	  value: 'created_at'
+  	}]
 	}),
 
 	async fetch() {
@@ -326,36 +321,8 @@ export default {
 		})
 
 		await this.$axios.get(`rooms/list`, { params: this.params }).then((response) => {
-			this.$store.commit('rooms/FETCH_ROOMS', {
-				rooms: response.data.data.map(item => ({
-					id: item.id,
-					title: item.title,
-					slug: item.slug,
-					rating: item.rating,
-					rakeback: item.rakeback,
-					bonus: item.bonus,
-					background: item.background,
-					restricted: item.restricted,
-					available: item.available,
-					blacklist: item.blacklist,
-					summary: item.summary,
-					claim_amount: item.claim_amount,
-					claim_currency: item.claim_currency ? {
-						code: item.claim_currency.code,
-						symbol: item.claim_currency.symbol
-					}: null,
-					image: {
-						filename: item.image.filename
-					},
-					network: {
-						title: item.network.title
-					},
-					tags: item.tags.map(tag => ({
-						title: tag.title
-					})),
-					review: item.review,
-				}))
-			})
+
+			this.$store.commit('rooms/FETCH_ROOMS', {rooms: response.data.data})
 
 			Object.keys(response.data).forEach(key => {
 				if (key !== 'data') {
@@ -403,13 +370,13 @@ export default {
 
 	methods: {
 
-	handleFilterChange(selected) {
-	  this.selected = selected.flatten
-	  Object.keys(selected.values).forEach(key => {
-		this[key] = selected.values[key]
-	  })
-	  this.fetchItems()
-	},
+  	handleFilterChange(selected) {
+  	  this.selected = selected.flatten
+  	  Object.keys(selected.values).forEach(key => {
+  		this[key] = selected.values[key]
+  	  })
+  	  this.fetchItems()
+  	},
 
 		async fetchItems(query) {
 
@@ -429,36 +396,9 @@ export default {
 			// })
 
 			await this.$axios.get(`rooms/list`, { params: this.params }).then((response) => {
-				this.$store.commit('rooms/FETCH_ROOMS', {
-					rooms: response.data.data.map(item => ({
-						id: item.id,
-						title: item.title,
-						slug: item.slug,
-						rating: item.rating,
-						rakeback: item.rakeback,
-						bonus: item.bonus,
-						background: item.background,
-						restricted: item.restricted,
-						available: item.available,
-						blacklist: item.blacklist,
-						summary: item.summary,
-						claim_amount: item.claim_amount,
-						claim_currency: item.claim_currency ? {
-							code: item.claim_currency.code,
-							symbol: item.claim_currency.symbol
-						}: null,
-						image: {
-							filename: item.image.filename
-						},
-						network: {
-							title: item.network.title
-						},
-						tags: item.tags.map(tag => ({
-							title: tag.title
-						})),
-						review: item.review,
-					}))
-				})
+
+        this.$store.commit('rooms/FETCH_ROOMS', {rooms: response.data.data})
+
 				Object.keys(response.data).forEach(key => {
 					if (key !== 'data') {
 						this[key] = response.data[key]
@@ -481,7 +421,6 @@ export default {
 		handlePageChange(number) {
 			this.page = number
 			this.fetchItems()
-			// history.replaceState({}, null, `${this.$route.path}/page/${number}`)
 		},
 
 		handleShowMore() {
