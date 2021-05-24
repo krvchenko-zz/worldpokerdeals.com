2c0b30ceb847b18a0b0677ea86529a2ea106bcaa
Your Name <you@example.com>
2021-05-25 01:53:27 +0700

    fix locale middleware

--- middleware/locale.js
+++ middleware/locale.js
@@ -1,19 +1,21 @@
 import { loadMessages } from '~/plugins/i18n'
 
 export default async ({ app, store, req, $axios }) => {
-	const host = req.headers.host
-	if (
-		host.substr(2, 1) === '.' &&
-		(host.substr(0, 2) === 'ru' || host.substr(0, 2) === 'es')
-	) {
-		store.dispatch('lang/setLocale', { locale: host.substr(0, 2) })
+	if (process.server) {
+		const host = req.headers.host
+
+		if (
+			host.substr(2, 1) === '.' &&
+			(host.substr(0, 2) === 'ru' || host.substr(0, 2) === 'es')
+		) {
+			store.dispatch('lang/setLocale', { locale: host.substr(0, 2) })
+		}
 	}
 
-	if (process.server) {
-		const locale = store.getters['lang/locale']
-		if (locale) {
-			$axios.setHeader('Accept-Language', locale)
-		}
+	const locale = store.getters['lang/locale']
+
+	if (locale) {
+		$axios.setHeader('Accept-Language', locale)
 	}
 
 	await loadMessages(store.getters['lang/locale'])
