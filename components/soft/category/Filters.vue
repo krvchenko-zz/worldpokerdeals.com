2d4ca6a836d46489565032faede22ff93137acd0
mazino <mazino@local>
2021-09-02 23:45:31 +0300

    refactor: extracted filters styles in one file.

--- components/soft/category/Filters.vue
+++ components/soft/category/Filters.vue
@@ -87,47 +87,4 @@
 	}
 </script>
 
-<style lang="scss">
-	$ico-filters: url('~assets/i/ico-filters.svg?data');
-
-	.filters {
-		margin-bottom: 40px;
-		border-radius: 4px;
-		overflow: hidden;
-		box-shadow: 0px 2px 0px rgba(198, 199, 202, 0.5);
-	}
-
-	.filters__label {
-		position: relative;
-		padding: 20px 24px;
-		font-family: 'Proxima Nova';
-		font-style: normal;
-		font-weight: bold;
-		font-size: 18px;
-		line-height: 24px;
-		letter-spacing: -0.2px;
-		color: #ffffff;
-		background: #2e3141;
-		&:after {
-			right: 20px;
-			top: 12px;
-			position: absolute;
-			content: '';
-			width: 33px;
-			height: 34px;
-			background: $ico-filters no-repeat center;
-		}
-	}
-
-	.filter {
-		border-top-left-radius: 4px;
-		border-top-right-radius: 4px;
-		border: 1px solid #e9e9e9;
-		border-bottom: 0;
-		background: #fafafa;
-		padding: 20px;
-		.filter-item {
-			padding: 0;
-		}
-	}
-</style>
+<style lang="scss"></style>
