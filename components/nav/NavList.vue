b2537425bbafd2f0af756910c37315c1e2c1f61d
Your Name <you@example.com>
2021-09-13 20:21:01 +0700

    fix horizontal scroll

--- components/nav/NavList.vue
+++ components/nav/NavList.vue
@@ -38,12 +38,10 @@
 			margin: 0;
 			border-radius: 4px;
 			display: flex;
-			overflow-x: scroll;
-			@include hide-scroll();
 			flex-basis: 100%;
 			flex-wrap: nowrap;
-			-webkit-overflow-scrolling: touch;
 			transition: transform 0.25s;
+			overflow-x: scroll;
 			@include hide-scroll();
 		}
 	}
