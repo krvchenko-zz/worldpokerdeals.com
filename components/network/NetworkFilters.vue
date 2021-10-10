9bf06f674f6e1ef9243f6b5ec79b6be72b1d5de3
Your Name <you@example.com>
2021-10-10 18:47:08 +0700

    fix networks

--- components/network/NetworkFilters.vue
+++ components/network/NetworkFilters.vue
@@ -7,14 +7,13 @@
 			</div>
 		</div>
 
-		<div v-if="showGeo" class="filters__geo">
-			<geo-switcher :value="country.code" :geo="geo" v-on="$listeners" />
-		</div>
+		<filter-geo class="filters__geo" v-on="$listeners" :geo="geo" />
 
 		<filter-dropdown
 			v-if="types.length"
 			:label="$t('room_type')"
 			icon="filter-room-type"
+			@open="$emit('filterOpen')"
 		>
 			<filter-item
 				v-for="(item, index) in types"
@@ -23,7 +22,7 @@
 			>
 				<checkbox
 					v-model="selected.types"
-					:value="item.value"
+					:value="item"
 					:label="item.label"
 					@change="handleFilterChange"
 				/>
@@ -32,21 +31,30 @@
 
 		<filter-dropdown
 			v-if="payments.length"
-			:label="$t('payments')"
+			:label="$t('payment_methods')"
 			icon="filter-payments"
+			@open="$emit('filterOpen')"
 		>
 			<filter-item
 				v-for="(item, index) in payments"
 				:key="index"
 				:count="item.count"
-				:icon="`${item.slug}-color-square`"
 			>
 				<checkbox
 					v-model="selected.payments"
-					:value="item.value"
+					:value="item"
 					:label="item.label"
 					@change="handleFilterChange"
 				/>
+				<template #icon>
+					<svg-icon
+						class="filter-item__icon"
+						:icon="`${item.slug}-color`"
+						:width="28"
+						:height="28"
+						view-box="0 0 30 30"
+					/>
+				</template>
 			</filter-item>
 		</filter-dropdown>
 
@@ -54,6 +62,7 @@
 			v-if="platforms.length"
 			:label="$t('platforms')"
 			icon="filter-platforms"
+			@open="$emit('filterOpen')"
 		>
 			<filter-item
 				v-for="(item, index) in platforms"
@@ -62,10 +71,20 @@
 			>
 				<checkbox
 					v-model="selected.platforms"
-					:value="item.value"
+					:value="item"
 					:label="item.label"
 					@change="handleFilterChange"
 				/>
+
+				<template #icon>
+					<svg-icon
+						class="filter-item__icon"
+						:icon="`${item.slug}-square`"
+						:width="28"
+						:height="28"
+						view-box="0 0 40 40"
+					/>
+				</template>
 			</filter-item>
 		</filter-dropdown>
 
@@ -73,6 +92,7 @@
 			v-if="licenses.length"
 			:label="$t('licenses')"
 			icon="filter-licenses"
+			@open="$emit('filterOpen')"
 		>
 			<filter-item
 				v-for="(item, index) in licenses"
@@ -81,7 +101,7 @@
 			>
 				<checkbox
 					v-model="selected.licenses"
-					:value="item.value"
+					:value="item"
 					:label="item.label"
 					@change="handleFilterChange"
 				/>
@@ -92,6 +112,7 @@
 			v-if="kycs.length"
 			:label="$t('verification')"
 			icon="filter-kyc"
+			@open="$emit('filterOpen')"
 		>
 			<filter-item
 				v-for="(item, index) in kycs"
@@ -99,11 +120,10 @@
 				:count="item.count"
 			>
 				<checkbox
-					v-model="selected.kyc"
-					:value="item.value"
+					v-model="selected.kyc.value"
 					:label="item.label"
 					:true-value="1"
-					:false-value="0"
+					:false-value="null"
 					@change="handleFilterChange"
 				/>
 			</filter-item>
@@ -113,6 +133,7 @@
 			v-if="tags.length"
 			:label="$t('room_features')"
 			icon="filter-room-features"
+			@open="$emit('filterOpen')"
 		>
 			<filter-item
 				v-for="(item, index) in tags"
@@ -121,7 +142,7 @@
 			>
 				<checkbox
 					v-model="selected.tags"
-					:value="item.value"
+					:value="item"
 					:label="item.label"
 					@change="handleFilterChange"
 				/>
@@ -139,121 +160,52 @@
 <script>
 	import { mapGetters } from 'vuex'
 	import filterMixin from '~/mixins/filterMixin'
-	import { deepCopy } from '~/utils'
 	import eventBus from '~/utils/event-bus'
 
 	export default {
+		name: 'RoomCategoryFilters',
+
 		components: {},
 
 		mixins: [filterMixin],
-
 		props: {
 			geo: {
-				required: true,
-			},
-
-			kycs: {
-				type: Array,
-			},
-
-			platforms: {
-				type: Array,
-			},
-
-			tags: {
-				type: Array,
-			},
-
-			payments: {
-				type: Array,
-			},
-
-			types: {
-				type: Array,
-			},
-
-			licenses: {
-				type: Array,
-			},
-
-			showGeo: {
-				type: Number,
+				type: [String, Boolean, Number],
 			},
 		},
 
 		data: () => ({
-			loading: false,
 			selected: {
-				kyc: [],
+				kyc: {
+					value: null,
+					label: 'Верификация',
+				},
 				platforms: [],
 				tags: [],
 				payments: [],
 				types: [],
 				licenses: [],
+				limits: [],
+				games: [],
+				disciplines: [],
+				networks: [],
+				hud: [],
+				certificates: [],
 			},
 		}),
 
-		updated() {},
-
-		created() {},
-
-		computed: {
-			...mapGetters({
-				locale: 'lang/locale',
-				country: 'location/country',
-			}),
-
-			cloneGeo() {
-				return this.geo
-			},
-
-			flatten() {
-				let items = []
-				Object.keys(this.selected).forEach(key => {
-					if (Array.isArray(this.selected[key]) && this.selected[key].length) {
-						for (let i = 0; i < this.selected[key].length; i++) {
-							const item = this[key].find(
-								el => el.value === this.selected[key][i]
-							)
-							items.push({ ...item, key })
-						}
-					}
-				})
-
-				return items
-			},
-
-			values() {
-				let items = {}
-				Object.keys(this.selected).forEach(key => {
-					if (Array.isArray(this.selected[key])) {
-						items[key] = this.selected[key]
-					}
-				})
-				return items
-			},
-		},
+		computed: {},
 
 		watch: {},
 
+		mounted() {
+			
+		},
+
 		methods: {
-			reset(data, clear) {
-				if (data.clear) {
-					this.selected = deepCopy(this.initial)
-					return false
-				}
-
-				if (Array.isArray(this.selected[data.key])) {
-					this.selected[data.key] = this.selected[data.key].filter(value => {
-						return value !== data.value
-					})
-				}
-			},
-
 			onClick() {
 				eventBus.$emit('filter:toggle', null)
 			},
-
 			clearFilters() {
 				eventBus.$emit('selected:delete', {
 					clear: true,
